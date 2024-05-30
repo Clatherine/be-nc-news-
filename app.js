0999c5202bd924964bdc9402696ac26f60310948
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-05-30 14:38:32 +0100

    added 'next' to penultimate app.use block

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use((err,req,res,next)=>{
     if(err.code === '22P02'){
         res.status(400).send({msg: "Invalid input: expected a number"})
     }
+    else(next(err))
 })
 
 app.use((err, req, res, next)=>{
