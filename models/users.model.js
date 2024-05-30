f33688677f00582ebd2b1de26281cefb84c9e2cc
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-05-30 15:29:42 +0100

    removed pg-format where not being used from models

--- models/users.model.js
+++ models/users.model.js
@@ -1,6 +1,5 @@
 const db = require('../db/connection')
 const fs = require('fs/promises')
-const format = require("pg-format")
 
 exports.checkUserExists = (username)=>{
     return db.query("SELECT username FROM users WHERE username = $1", [username])
